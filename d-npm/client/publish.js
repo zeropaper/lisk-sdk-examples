5907705b79dbdeab3f0892c4259716799d2f6677
Mona Baerenfaenger <mona@lightcurve.io>
2019-08-02 16:09:33 +0200

    Make publish script work

--- d-npm/client/publish.js
+++ d-npm/client/publish.js
@@ -1,6 +1,5 @@
-const {
-    flags: { endpoint, port, protocol },
-} = this.parse(PackagePublishCommand);
+const ipfsClient = require('ipfs-http-client');
+const path = require('path');
 const workingDirectory = process.cwd();
 const pkg = require(path.join(workingDirectory, './package.json'));
 if (!pkg) {
@@ -8,17 +7,41 @@ if (!pkg) {
         'package.json not found. Please run the command in the root folder',
     );
 }
-console.log({ endpoint, port, protocol });
-const ipfs = ipfsClient(endpoint, port, { protocol });
-const filesAdded = await ipfs.addFromFs(workingDirectory, {
-    recursive: true,
-    ignore: [
-        'src/**',
-        'test/**',
-        'node_modules/**',
-        'tslint.json',
-        'tsconfig.json',
-    ],
-});
-console.log('Added files:', filesAdded);
-console.log('Last Item:', filesAdded.reverse()[0]);
+
+const ipfs = ipfsClient('ipfs.infura.io', '5001', { protocol: 'https' });
+
+
+
+async function run()
+{
+    try {
+        const filesAdded = await ipfs.addFromFs(workingDirectory, {
+            recursive: true,
+            ignore: [
+                'src/**',
+                'test/**',
+                'node_modules/**',
+                'tslint.json',
+                'tsconfig.json',
+            ],
+        });
+
+        console.log('Added files:', filesAdded);
+        console.log('Last Item:', filesAdded.reverse()[0]);
+    } catch (err) {
+        console.log("ERROR!!!!");
+        console.log(err);
+    }
+
+}
+
+run().catch(function(error) {
+        console.log("ERROOOOR");
+        console.log(error);
+    }).then( function (result) {
+        console.log("FINISHED");
+        console.log(result);
+        //console.log('Last Item:', filesAdded.reverse()[0]);
+    });
+
+
